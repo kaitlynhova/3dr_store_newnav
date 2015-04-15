3f867f6eb37b73eeb51b82a4985e61e3cf6c1cac
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-04-15 10:53:21 -0700

    new search styles and caret easter egg

--- js/fancy-dropdown.js
+++ js/fancy-dropdown.js
@@ -3,4 +3,12 @@ $( document ).ready(function() {
     $( "#product-list" ).slideToggle();
     $( "#mobile-product-list" ).slideToggle();
   });
+
+  $( "#desktop-search-button" ).click(function() {
+    $( "#poppin-search-bar" ).slideToggle();
+  });
+
+  $( "#products" ).click(function() {
+    $( "#caret" ).toggleClass('spincaret');
+  });
 });
